72abd2d9b888d2a03f17bdcd92084c35f598a51a
amirisback <faisalamircs@gmail.com>
2019-10-15 22:59:24 +0700

    update: change JunkAppsViewAdapter to Kotlin

---
+++ app/src/main/java/com/frogobox/cleaner/view/adapter/JunkAppsViewAdapter.kt
@@ -0,0 +1,37 @@
+package com.frogobox.cleaner.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.frogobox.cleaner.R
+import com.frogobox.cleaner.model.Apps
+import kotlinx.android.synthetic.main.content_item_junk.view.*
+
+/**
+ * Created by Frogobox Software Industries 2/25/2017.
+ */
+
+class JunkAppsViewAdapter(private val apps: List<Apps>) : RecyclerView.Adapter<JunkAppsViewAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.content_item_junk, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val (size, image) = apps[position]
+        holder.size.text = size
+        holder.image.setImageDrawable(image)
+    }
+
+    override fun getItemCount(): Int {
+        return apps.size
+    }
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val size = view.apptext
+        val image = view.appimage
+    }
+
+}
